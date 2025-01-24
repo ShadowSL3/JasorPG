237eeeaaf77563cc8ad583a6377df43b8a5e5767
ShadowSL3 <169302752+ShadowSL3@users.noreply.github.com>
2025-01-24 15:52:17 +0000

    Update Node.cs Adding Namespace

--- Assets/Script/Ai/Node.cs
+++ Assets/Script/Ai/Node.cs
@@ -1,13 +1,11 @@
-
 using UnityEngine;
-
+namespace JasorPG.Utils.Node;
 public class Node 
 {
     public bool walkable;
     public Vector3 worldPosition;
     public int gridX;
     public int gridY;
-
     public int gCost;
     public int hCost;
     public Node parent;
